5c70f697a8c48943a96c7437d0b859c1785a91f4
kef <kekekefir@gmail.com>
2023-04-30 15:27:24 +0300

    done 2.0

--- logreg.py
+++ logreg.py
@@ -86,7 +86,7 @@ if (input_file is not None) and input_file.name.endswith(".csv"):
         precision_test = test.loc[(test['y'] == test['y^'])].shape[0]/ test.shape[0]*100
         #st.write(f'Точность предсказания: {precision}%')
 
-        sns.lineplot(data = test.sort_values('y_sigm').reset_index()[['y_sigm', 'y_pred']])
+        sns.lineplot(data = test.sort_values('y_sigm').reset_index()[['y_sigm', 'y_^']])
         plt.axhline(0.5, color='r', label='0.5')
 
 
