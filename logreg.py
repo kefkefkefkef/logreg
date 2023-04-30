5ffc82402781d216675460cc292ffc54fb2d12b7
kef <kekekefir@gmail.com>
2023-04-30 14:03:17 +0300

    done 2.0

--- logreg.py
+++ logreg.py
@@ -48,28 +48,27 @@ if (input_file is not None) and input_file.name.endswith(".csv"):
     xs = st.multiselect('Выберите показатели для вычисления весов:', (train.columns))
    
     if st.button("Поехали"):
-     ss = StandardScaler()
-     train[xs] = ss.fit_transform(train[xs])
-     logreg = LogReg(0.01)
-     logreg.fit(train[xs], train[y].to_numpy())     
-     #st.write('You selected:', option)
-     
-     st.write('Веса модели:', logreg.w, 'Свободный член:', f'{logreg.bias}')   
-     prediction = logreg.predict(train[xs])
-    
-     
-     st.write('Сверим предсказание модели с входными данными:')
-     compare_train = pd.DataFrame(data={'y': train[y], 'y^': prediction})
+        ss = StandardScaler()
+        train[xs] = ss.fit_transform(train[xs])
+        logreg = LogReg(0.01)
+        logreg.fit(train[xs], train[y].to_numpy())     
+        #st.write('You selected:', option)
+        
+        st.write('Веса модели:', logreg.w, 'Свободный член:', f'{logreg.bias}')   
+        prediction = logreg.predict(train[xs])
+        
+        
+        st.write('Сверим предсказание модели с входными данными:')
+        compare_train = pd.DataFrame(data={'y': train[y], 'y^': prediction})
 
-     def compare(s):
-        return ['background-color: #90EE90']*len(s) if s['y'] == s['y^'] else ['background-color: #FFCCCB']*len(s)
+        def compare(s):
+            return ['background-color: #90EE90']*len(s) if s['y'] == s['y^'] else ['background-color: #FFCCCB']*len(s)
 
-     st.dataframe(compare_train.style.apply(compare, axis=1))
-     precision = compare_train.loc[(compare_train['y'] == compare_train['y^'])].shape[0]/ compare_train.shape[0]*100
-     st.write(f'Точность предсказания: {precision}%')
+        st.dataframe(compare_train.style.apply(compare, axis=1))
+        precision = compare_train.loc[(compare_train['y'] == compare_train['y^'])].shape[0]/ compare_train.shape[0]*100
+        st.write(f'Точность предсказания: {precision}%')
 
-     input_file2 = st.file_uploader("Загрузите данные для предсказания",type=['csv'])
-     if (input_file2 is not None) and input_file2.name.endswith(".csv"):
+        input_file2 = st.file_uploader("Загрузите данные для предсказания",type=['csv'])
         test = pd.read_csv(input_file2).drop('Unnamed: 0', axis=1)
         test[xs] = ss.fit_transform(test[xs])
         test['y^'] = logreg.predict(test[xs])
